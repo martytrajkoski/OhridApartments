ae1d589ed3d4fcf040725f96648f67b0f0d73838
martytrajkoski <martytrajkoski@gmail.com>
2025-05-18 17:30:51 +0200

    api change

--- src/axios/axiosClient.ts
+++ src/axios/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://api.theohridapartments.com/api",
     headers: {
         "Content-type": 'application/json',
     },
